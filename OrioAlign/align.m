01873b609d0919e7ef2536b2238ee6b165e4c7b1
Chutlhu <diego.dicarlo89@gmail.com>
2016-06-02 03:28:33 +0200

    add text to plots

--- OrioAlign/align.m
+++ OrioAlign/align.m
@@ -14,7 +14,7 @@ addpath(genpath('./'))
 % NB:   Thresholds for merging events:
 %               thrs_msec_note, thrs_msec_rest
 %       Number of sustain states in the network:
-%               nSustStates 
+%               nSustStates
 %       Number of filters in the filterbanalysisParamsk:
 %               nFilters
 %       Analysis Parameters:
@@ -52,10 +52,18 @@ audioPathName = 'audio\';
 %   zz = zz + 1;
 % end
 
-%% Create Hidding Markov Model
-% bank of filters for HMM
+
+%% Bank of filters for HMM
 fb = makeFB(score, nFilters, analysisParams);
 
+figure()
+imagesc(fb.bands(:, 1:500)')
+set(gca, 'YDir', 'Normal');
+title('Spectrogram generated from the MIDI score');
+xlabel('Time (s)');
+ylabel('Frequencies (Hz)');
+
+%% Create Hidding Markov Model
 % NB:   nSustStates:
 %           numero stati sustain per simulare la durata di una nota (2 - 4 stati)
 %       analysisParams
@@ -75,25 +83,22 @@ probs = zeros(1,6);
 recognize; % compute alfa, beta e gamma
 
 %% Results
-
 % filters normalized output for each observation
 filtersOutput = zeros(nFrameAudio,1);
 % resulting sequence of states
 statesSequences = hmm.obs(decodedModel_scaledGamma, 2);
 
 for m = 1:nFrameAudio
-  if statesSequences(m) > 0
-    filtersOutput(m) = harmonicContents(statesSequences(m), m);
-  end
+    if statesSequences(m) > 0
+        filtersOutput(m) = harmonicContents(statesSequences(m), m);
+    end
 end
 
 sumFiltOutput = filtersOutput(filtersOutput > 0);   % output energy from state
-maxFiltOutput = max(harmonicContents)';              % for evert bank the max over time of its throughput
-meanFiltOutput = mean(harmonicContents)';            % mean over time the mean over time of its throughput
+maxFiltOutput = max(harmonicContents)';             % for evert bank the max over time of its throughput
+meanFiltOutput = mean(harmonicContents)';           % mean over time the mean over time of its throughput
 
-%% Show results
-% qui lo stampa solo, ma e` l'info che utilizzo per l'allineamento
-
+%% Show performance plot
 % grafico piu` promettente
 % alfa e` un "ottimo locale" => ok per realtime
 % beta informazione futura
@@ -105,115 +110,146 @@ meanFiltOutput = mean(harmonicContents)';            % mean over time the mean o
 
 % sgamma => (se so che iniziano e finisco allora stesso tempo (score e
 % musica) - informazione in piu` che sto utilizzando
+%% Decoded Model using scaled gamma
 figure()
 fldm = imagesc(log(real(scaledGamma)));
 pldm = get(fldm, 'parent');
 axis xy
 hold on; plot(decodedModel_scaledGamma,'ok'); hold off;
 title('S-gamma probabilities');
-xlabel('Audio waveform frames');
-ylabel('HMM states');
+xlabel('Audio frames (s)');
+ylabel('HHM states (notes)');
 
-% Probability histogram: performance del banco dei filtri
+
+%% Probability histogram: filterbanks performance
 % quello che sento vs quello che vedo se modello con banco dei filtri
 % idealmente htot dovrebbe essere molto simile a mtot;
 figure()
-subplot(3,1,1)
-title('Output probability: energy from a filter at given time');
+subplot(2,1,1)
+title('Output probability from filterbanks in the original song');
 hist([0; sumFiltOutput; 1], 100)
+xlabel('Probabilities');
+ylabel('Empirical Comulative Density Function (eCDF)');
 % max output prob banco di filtri
-subplot(3,1,2)
-title('max (over time) of output probability: energy from filter bank');
+subplot(2,1,2)
+title('Output probability from filterbanks in the HMM model');
 hist([0; maxFiltOutput; 1], 100)
+xlabel('Probabilities');
+ylabel('Empirical Comulative Density Function (eCDF)');
 % mean
-subplot(3,1,3)
-title('mean (over time) of output probability: energy from a filter');
-hist([0; meanFiltOutput; 1], 100)
+%subplot(3,1,3)
+%title('mean (over time) of output probability: energy from a filter');
+%hist([0; meanFiltOutput; 1], 100)
 
-%%%%%%%%%%%%%%%%
-% Score analysisParamsd FFTs
+%% Score analysis and FFTs
 figure()
 subplot(3,1,1)
 fpos = firstOnset;
-    for n = 1:length(score)
-        ipos = fpos;
-        fpos = ipos + score{n, 2};
-
-        notes = score{n,1};
-        for m = 1:length(notes)
-            nt = notes(m);
-            line([ipos,fpos],[nt,nt],'LineWidth',3,'LineStyle','-','Color',[0 0 0])
-        end
+for n = 1:length(score)
+    ipos = fpos;
+    fpos = ipos + score{n, 2};
+    
+    notes = score{n,1};
+    for m = 1:length(notes)
+        nt = notes(m);
+        line([ipos,fpos],[nt,nt],'LineWidth',3,'LineStyle','-','Color',[0 0 0])
     end
-title('MIDI pianoo roll of the score')  
+end
+title('MIDI piano roll of the score')
 
-[void, ppp] = max(-diff(fb.extrs'));
-ppp = max(ppp)+50;
-fy = zeros(size(ySpectrogram));
-for m = 1:length(notes)
-    if hmm.obs(decod_sg(m), 1),
-      fy(:,m) = ySpectrogram(:, m) .* fb.bands(hmm.obs(decod_sg(m),2), :)';
+[~, ppp] = max(-diff(fb.extrs'));
+ppp = max(ppp)+150;
+decodedModelOverSpectrogram = zeros(size(ySpectrogram));
+decodedModel = zeros(size(ySpectrogram));
+for m = 1:nFrameAudio
+    if hmm.obs(decodedModel_scaledGamma(m), 1) % it is not a rest
+        decodedModel(:, m) = fb.bands(hmm.obs(decodedModel_scaledGamma(m),2),:);
+        decodedModelOverSpectrogram(:, m) = ySpectrogram(:, m) .* decodedModel(:,m);
     end
 end
 
 subplot(3,1,2)
 imagesc(log(ySpectrogram(1:ppp,:)))
-
 title('Log-magnitude of the song')
+set(gca, 'YDir', 'Normal');
+title('Spectrogram of the song');
+xlabel('Frames (s)');
+ylabel('Frequencies (Hz)');
+colormap('bone');
+
 subplot(3,1,3)
-imagesc(log(fy(1:ppp,:) + ySpectrogram(1:ppp,:) / 10000))
-title('Log-magnitude of ???')
+imagesc(log(decodedModelOverSpectrogram(1:ppp,:) + ySpectrogram(1:ppp,:) / 10000))
+set(gca, 'YDir', 'Normal');
+title('Alligned score over spectrogram')
+xlabel('Frames (s)');
+ylabel('Frequencies (Hz)');
 
-%%%%%%%%%%%%%%%%%%
-% Parameters trend
 
-% ALFA
-figure(3)
-subplot(3,1,1)
+%% Parameters trend
+
+% alpha
+figure()
+subplot(3,2,1)
 hold off;
 imagesc(log(real(alfa)))
 axis xy
-hold on;
-plot(decod_a,'ok')
+hold on; plot(decodedModel_alpha,'ok'); hold off;
+title('Alligment using ALPHA probabilities')
+xlabel('Audio frames (s)');
+ylabel('HHM states (notes)');
 
-% DELTA
-subplot(3,1,2)
+% delta
+subplot(3,2,3)
 hold off;
 imagesc(delta)
 axis xy
-hold on;
-plot(decod_d,'ok')
+hold on; plot(decodedModel_delta,'ok'); hold off;
+title('Alligment using DELTA probabilities')
+xlabel('Audio frames (s)');
+ylabel('HHM states (notes)');
 
-% GAMMA
-subplot(3,1,3)
+% gamma
+subplot(3,2,5)
 hold off;
 imagesc(log(real(gamma)))
 axis xy
-hold on;
-plot(decod_g,'ok')
+hold on; plot(decodedModel_gamma,'ok');
+title('Alligment using GAMMA probabilities')
+xlabel('Audio frames (s)');
+ylabel('HHM states (notes)');
 
-figure(4)
-subplot(3,1,1)
+% log alpha
+subplot(3,2,2)
 hold off;
 imagesc(log(real(alfa)))
 axis xy
 hold on;
-plot(decod_a,'ok')
+plot(decodedModel_alpha,'ok')
+title('Alligment using LOG ALPHA probabilities')
+xlabel('Audio frames (s)');
+ylabel('HHM states (notes)');
 
-subplot(3,1,2)
+% scaled delta
+subplot(3,2,4)
 hold off;
 imagesc(delta)
 axis xy
 hold on;
-plot(decod_sd,'ok')
+plot(decodedModel_scaledDelta,'ok')
+title('Alligment using SCALED DELTA probabilities')
+xlabel('Audio frames (s)');
+ylabel('HHM states (notes)');
 
-subplot(3,1,3)
+% scaled gamma
+subplot(3,2,6)
 hold off;
-imagesc(log(real(sgamma)))
+imagesc(log(real(scaledGamma)))
 axis xy
 hold on;
-plot(decod_sg,'ok')
-
+plot(decodedModel_scaledGamma,'ok')
+title('Alligment using SCALED GAMMA probabilities')
+xlabel('Audio frames (s)');
+ylabel('HHM states (notes)');
 
 
 
